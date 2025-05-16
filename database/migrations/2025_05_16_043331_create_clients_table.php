9c5d87be1675e806d9c3875b24a180577645948d
Jay <atmajaya.dw@gmail.com>
2025-05-16 17:40:07 +0700

    Create CMS menu Category

--- database/migrations/2025_05_16_043331_create_clients_table.php
+++ database/migrations/2025_05_16_043331_create_clients_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('client_id');
             $table->date('date');
             $table->softDeletes();
+            $table->timestamps();
         });
     }
 
